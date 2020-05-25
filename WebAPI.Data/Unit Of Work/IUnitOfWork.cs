bc284573a63a059d82192442c0682801ed792cd6
hai.buidinh <hai.buidinh@vti.com.vn>
2020-05-25 15:42:04 +0700

    commit

---
+++ WebAPI.Data/Unit Of Work/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model.UnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        Task Commit();
+    }
+}
---
+++ WebAPI.Data/Unit Of Work/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebAPI.Model.DataContext;
+
+namespace WebAPI.Model.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool disposed = false;
+        private readonly APIDataContext _dbcontext;
+        public UnitOfWork(APIDataContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+
+        }
+
+         public async Task Commit()
+        {
+            await _dbcontext.SaveChangesAsync();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                if(disposing)
+                {
+                    _dbcontext.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
